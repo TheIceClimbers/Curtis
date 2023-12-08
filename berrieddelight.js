8f1f144f81a3eb44b179fbd9aea2898e0863ff10
TheIceClimbers <144732067+TheIceClimbers@users.noreply.github.com>
2023-12-08 13:49:40 -0700

    Update berrieddelight.js

--- berrieddelight.js
+++ berrieddelight.js
@@ -1,2 +1,18 @@
 alert("This is a bucket. Dear God... There's more. No... It contains a bucket. Dear God... There's more. No... It contains a bucket. Dear God... There's more. No... It contains a bucket.");
 console.log("I hope you enjoy learning JavaScript!");
+
+let person = {
+  name: "AmongUsImposter",
+  age: 69,
+  favoriteColor: "Pink"
+};
+
+function printToConsole(input) {
+  window.alert(person.name+ person.age+ person.favoriteColor);
+  console.log(person);
+}
+
+function birthday() {
+  person.age = person.age+1;
+  window.alert(person.age);
+}
